a0281186a6b93dcf9507123b94f595a26cd02cad
Sarah Drake <jussaradrake@outlook.com>
2019-08-29 09:34:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+
+class School
+  attr_reader :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = ["My new key"] << "hi cliff"
+    # {"my new key" => ["hi cliff"]}
+    @roster = Hash.new {|h, k| h[k] = []}
+end
+
+def roster
+  @roster
+end
+
+end
